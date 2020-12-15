80ac0ae1dbe12add8e53addafd09f5063a96a2ae
EpifanioD <daviepifaniomaia@hotmail.com>
2020-12-15 15:55:05 -0300

    first commit

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,35 @@
+import { request, response, Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+interface User {
+  name: string;
+  email: string;
+  password?: string;
+}
+
+const usersRouter = Router();
+
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password} = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user: User = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+
+});
+
+export default usersRouter;
